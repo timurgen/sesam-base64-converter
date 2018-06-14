14f3f5579d14e208129ca598382d5aee66f6ca78
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-06-14 13:47:23 +0200

    docs

--- service/base64transform_service.py
+++ service/base64transform_service.py
@@ -1,26 +1,38 @@
-from flask import Flask, request, Response
+"""Micro service for Sesam applications that converts data to base64"""
+import json
 import base64
 import os
 
-app = Flask(__name__)
+from flask import Flask, request, Response
+
+APP = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@APP.route("/", methods=["POST"])
 def encode_payload():
-    payload_to_convert = request.data;
+    """micro service entry point"""
+    payload_to_convert = request.data
     return Response(
-        encode(payload_to_convert),
-        mimetype='application/base64'
+        generate_json(encode(payload_to_convert)),
+        mimetype='application/json'
     )
 
 
+def generate_json(data):
+    """generates json entity that can be consumed by sesam instance"""
+    print(type(data))
+    return json.dumps({"_id": data.decode("utf-8")})
+
+
 def encode(data):
+    """encode given data as base64"""
     return base64.b64encode(data)
 
 
 def decode(base64_data):
+    """decode base64 string"""
     return base64.b64decode(base64_data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('PORT', 5000))
+    APP.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('PORT', 5000))
