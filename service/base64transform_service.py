023a4d8bfd78b94178c48ce3fb70d846bb8f4bdf
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-06-14 11:05:18 +0200

    changed default port according to exposed docker

--- service/base64transform_service.py
+++ service/base64transform_service.py
@@ -23,4 +23,4 @@ def decode(base64_data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('PORT', 5000))
