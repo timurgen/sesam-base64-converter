fd07af697a2ac19d94277f91cddf0e3bff033201
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-06-19 11:28:53 +0200

    convert only data from one property instead of whole payload. Property name assigned as env variable PAYLOAD_PROPERTY and is 'entities' by default

--- service/base64transform_service.py
+++ service/base64transform_service.py
@@ -3,29 +3,38 @@ import json
 import base64
 import os
 import logging
-
-from flask import Flask, request, Response
+from pprint import pprint
+from flask import Flask, request, Response, abort
 
 APP = Flask(__name__)
 
 LOG_LEVEL = logging.getLevelName(os.environ.get('LOG_LEVEL', 'INFO'))
-logging.basicConfig(level=LOG_LEVEL) # dump log to stdout
+logging.basicConfig(level=LOG_LEVEL)  # dump log to stdout
+PAYLOAD_PROPERTY = os.environ.get('PAYLOAD_PROPERTY', 'entities')
 
 
-@APP.route("/", methods=["GET"])
+@APP.route("/", methods=["GET", "POST"])
 def encode_payload():
     """micro service entry point"""
-    logging.info("Serving request from %s", request.remote_addr)
-    payload_to_convert = request.data
+    logging.info("Serving request from %s with content-type: %s", request.remote_addr,
+                 request.content_type)
+    if "application/json" == request.content_type or "text/plain" == request.content_type:
+        payload_to_convert = request.get_json(force=True)
+    else:
+        abort(415)  # unsupported media type
+
+    if not payload_to_convert:
+        abort(500)
+
     return Response(
-        generate_json(encode(payload_to_convert)),
+        generate_json(encode(payload_to_convert.get(PAYLOAD_PROPERTY).encode("utf-8"))),
         mimetype='application/json'
     )
 
 
 def generate_json(data):
     """generates json entity that can be consumed by sesam instance"""
-    return json.dumps({"_id": data.decode("utf-8")})
+    return json.dumps({PAYLOAD_PROPERTY: data.decode("utf-8")})
 
 
 def encode(data):
